0b5a229051b980152804149c2b216f79793a1b0c
aishwarya <aishwarya.shirse@gmail.com>
2019-12-27 15:16:23 +0530

    add msgs

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,6 +3,9 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println ( "hello world" );
+        System.out.println ( "good afternoon" );
+        System.out.println("good afternoon");
 	// write your code here
     }
 }
